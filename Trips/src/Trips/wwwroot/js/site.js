037edf118b4a634c892b3941f9802756ae4dce24
Desislav Petrov <petrov_desislav@abv.bg>
2017-08-23 22:36:12 +0300

    added sidebar toggle

---
+++ Trips/src/Trips/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    var element = $("#username");
+    element.text = "Desislav Petrov";
+
+    var main = $("#main");
+    main.on("mouseenter", function(){
+            main.style = "background-color: #888";
+    });
+
+    main.on("mouseleave", function () {
+        main.style = "";
+    });
+        
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+    })
+})();
