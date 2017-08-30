f5d9d8c53b6bcbeadbfedac64b331993f4f215b3
Desislav Petrov <petrov_desislav@abv.bg>
2017-08-31 01:58:57 +0300

    added ModelState and config.json

--- Trips/src/Trips/Controllers/Web/AppController.cs
+++ Trips/src/Trips/Controllers/Web/AppController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Trips.ViewModels;
+using Trips.Services;
+using Microsoft.Extensions.Configuration;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,8 +13,20 @@ namespace Trips.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService mailService;
+        private IConfigurationRoot config;
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            this.mailService = mailService;
+            this.config = config;
+        }
         // GET: /<controller>/
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
         {
             return View();
         }
@@ -20,6 +34,17 @@ namespace Trips.Controllers.Web
         [HttpPost]
         public IActionResult Contact (ContactViewModel model)
         {
+            if (model.Email.Contains("asd.com"))
+            {
+                ModelState.AddModelError("", "We don't support ASD addresses");
+            }
+            if (ModelState.IsValid)
+            {
+                this.mailService.SendMail(this.config["MailSettings:ToAddress"], model.Email, "From Trips", model.Message);
+                ModelState.Clear();
+                ViewBag.UserMessage = "Message Sent";
+            }
+            
             return View();
         }
 
--- Trips/src/Trips/Startup.cs
+++ Trips/src/Trips/Startup.cs
@@ -7,15 +7,39 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Trips.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace Trips
 {
     public class Startup
     {
+        private IHostingEnvironment environment;
+        private IConfigurationRoot config;
+        public Startup(IHostingEnvironment environment)
+        {
+            this.environment = environment;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(this.environment.ContentRootPath)
+                .AddJsonFile("config.json");
+
+            this.config = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(this.config);
+            if (this.environment.IsEnvironment("Development") || this.environment.IsEnvironment("Testing"))
+            {
+                services.AddScoped<IMailService, DebugMailService>();
+            }
+            else
+            {
+                // Implement a real Mail Service
+            }
+
             services.AddMvc();
         }
 
