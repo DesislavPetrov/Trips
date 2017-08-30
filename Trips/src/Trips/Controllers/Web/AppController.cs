efe72d159827aafa87d9e8924b1df5cbb698aca4
Desislav Petrov <petrov_desislav@abv.bg>
2017-08-30 23:58:23 +0300

    added validation for the Contact form

--- Trips/src/Trips/Controllers/Web/AppController.cs
+++ Trips/src/Trips/Controllers/Web/AppController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Trips.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,7 +17,8 @@ namespace Trips.Controllers.Web
             return View();
         }
 
-        public IActionResult Contact ()
+        [HttpPost]
+        public IActionResult Contact (ContactViewModel model)
         {
             return View();
         }
